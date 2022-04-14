0f537138588e09db60fb085bcc9a6abb60a799c8
Toushik Banik <toushikbanikeducation@gmail.com>
2022-04-14 17:08:22 +0530

    Orientation Set on landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
   runApp(const MyApp());
 }
 
